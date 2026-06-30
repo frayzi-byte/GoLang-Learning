e606ec49e34170f3991b88bed212fb0ffa118aa0
frayzi-byte <mrloloboy@gmail.com>
2026-06-30 18:20:25 +0500

    HELLO SCORE

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello, World!")
+	text := "Game Over"
+	score := 111
+
+	fmt.Println(score)
+	fmt.Println(text)
+	text = "Game Paused"
+	score = 50
+
+	fmt.Println(score)
+	fmt.Println(text)
+
+	score = 50 + 10
+	fmt.Println(score)
+
+	score = score + 10
+	fmt.Println(score)
 }
+
+///ВАЖНО! В GoLang очень строгая типизация, и переменная записанная как текст не может быть изменена в число
+///Пример:
+///text:= "Game Over"
+///text=30
+///ЭТО ОШИБКА
